ead25fd10cf8e5c638acac4c35e30bd17ee3ecfc
Emmanuel Buccheri <emmanuel.buccheri@webjump.com.br>
2022-08-16 17:37:38 -0300

    Update checkoutElements.js

--- cypress/support/elements/checkoutElements.js
+++ cypress/support/elements/checkoutElements.js
@@ -1,11 +1,14 @@
-export const ELEMENTS = {
-    stepTitle:'.step-title',
-    goToPaymentButton:'.button.action.continue.primary',
-    placeOrderButton:'.action.primary.checkout',
-    streetField:'input[name="street[0]"]',
-    countrySelect:'select[name="country_id"]',
-    regionSelect:'select[name="region_id"]',
-    cityField:'input[name="city"]',
-    postalCodeField:'input[name="postcode"]',
-    telephoneField:'input[name="telephone"]'
+class checkoutElements {
+
+    stepTitle= () => { return '.step-title'}
+    goToPaymentButton= () => { return '.button.action.continue.primary'}
+    placeOrderButton= () => { return '.action.primary.checkout'}
+    streetField= () => { return 'input[name="street[0]"]'}
+    countrySelect= () => { return 'select[name="country_id"]'}
+    regionSelect= () => { return 'select[name="region_id"]'}
+    cityField= () => { return 'input[name="city"]'}
+    postalCodeField= () => { return 'input[name="postcode"]'}
+    telephoneField= () => { return 'input[name="telephone"]'}
+
 }
+export default checkoutElements;
