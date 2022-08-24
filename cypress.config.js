515e901306e9060a91f79e0225fc54ba4b8af681
Renan Maia Fernandes <renan.fernandes@webjump.com.br>
2022-08-24 16:46:21 -0300

    update package

--- cypress.config.js
+++ cypress.config.js
@@ -12,6 +12,10 @@ module.exports = defineConfig({
   viewportWidth: 1440,
   viewportHeight: 900,
   defaultCommandTimeout: 25000,
+  video: false,
+  reports: false,
+  screenshots: false,
+  downloads: false,
   retries: {
     runMode: 2,
     openMode: 0,
